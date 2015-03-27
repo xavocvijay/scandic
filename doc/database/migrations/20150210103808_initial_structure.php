5bde685c4327a2ead66c1f780010acc7afe7b6e7
Vadym <vadym@agile55.com>
2015-03-27 13:37:56 +0200

    clean up migrations

--- doc/database/migrations/20150210103808_initial_structure.php
+++ doc/database/migrations/20150210103808_initial_structure.php
@@ -22,14 +22,12 @@ class InitialStructure extends AbstractMigration
      */
     public function up()
     {
-        $table = $this->table('settings');
-        $table
+        $this->table('settings')
             ->addColumn('key', 'string',['limit'=>'255','null'=>false])
             ->addColumn('value', 'string',['limit'=>'255','null'=>false])
             ->create();
 
-        $table = $this->table('user');
-        $table
+        $this->table('user')
             ->addColumn('email', 'string',['limit'=>'255','null'=>false])
             ->addColumn('password', 'string',['limit'=>'255','null'=>false])
             ->addColumn('name', 'string',['limit'=>'255','null'=>true])
@@ -37,44 +35,6 @@ class InitialStructure extends AbstractMigration
             ->addColumn('is_deleted', 'integer',['null'=>false,'default'=>'0'])
             ->create();
 
-        $table = $this->table('page');
-        $table
-            ->addColumn('title', 'string',['limit'=>'255','null'=>false])
-            ->addColumn('menu_type', 'string',['limit'=>'255','null'=>false])
-            ->addColumn('type', 'string',['limit'=>'255','null'=>false])
-            ->addColumn('has_content', 'integer',['null'=>false,'default'=>'0'])
-            ->addColumn('has_sub_pages', 'integer',['null'=>false,'default'=>'0'])
-            ->addColumn('hash_url', 'string',['limit'=>'255','null'=>false])
-            ->addColumn('order', 'integer',['limit'=>'11','null'=>true,'default'=>null])
-            ->addColumn('meta_keywords', 'string',['limit'=>'255','null'=>true,'default'=>null])
-            ->addColumn('meta_description', 'string',['limit'=>'255','null'=>true,'default'=>null])
-            ->addColumn('created_dts', 'datetime',['null'=>false])
-            ->addColumn('is_deleted', 'integer',['null'=>false,'default'=>'0'])
-            ->addColumn('page_id', 'integer',['limit'=>'11','null'=>true])
-            ->create();
-
-        $table = $this->table('block');
-        $table
-            ->addColumn('system_name', 'string',['limit'=>'255','null'=>false])
-            ->addColumn('type', 'string',['limit'=>'255','null'=>false])
-            ->addColumn('content', 'text',['null'=>true,'default'=>null])
-            ->addColumn('page_id', 'integer',['limit'=>'11','null'=>false])
-            ->addColumn('order', 'integer',['limit'=>'11','null'=>true,'default'=>null])
-            ->addColumn('language_id', 'integer',['limit'=>'11','null'=>false])
-            ->addColumn('created_dts', 'datetime',['null'=>false])
-            ->addColumn('is_deleted', 'integer',['null'=>false,'default'=>'0'])
-            ->create();
-
-        $table = $this->table('language');
-        $table
-            ->addColumn('lang_code', 'string',['limit'=>'255','null'=>false])
-            ->create();
-
-        $table = $this->table('search');
-        $table
-            ->addColumn('content', 'string',['limit'=>'255','null'=>false])
-            ->addColumn('block_id', 'integer',['limit'=>'11','null'=>false])
-            ->create();
     }
 
     /**
@@ -82,22 +42,8 @@ class InitialStructure extends AbstractMigration
      */
     public function down()
     {
-        $table = $this->table('settings');
-        $table->drop();
+        $this->table('settings')->drop();
+		$this->table('user')->drop();
 
-        $table = $this->table('user');
-        $table->drop();
-
-        $table = $this->table('page');
-        $table->drop();
-
-        $table = $this->table('block');
-        $table->drop();
-
-        $table = $this->table('language');
-        $table->drop();
-
-        $table = $this->table('search');
-        $table->drop();
     }
 }
