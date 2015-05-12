bf91b3c95528aa660add4fc64d30044f69950ec0
Romans Malinovskis <romans@agiletoolkit.org>
2015-05-12 17:54:27 +0100

    format 2buttons nicer

--- frontend/lib/View/Actions.php
+++ frontend/lib/View/Actions.php
@@ -1,16 +1,22 @@
     <?php
 class View_Actions extends View {
-    public $sub=[1,2];
     function init(){
         parent::init();
     }
     function setModel($m){
         $m = parent::setModel($m);
 
-        foreach($this->sub as $x){
-            if($m['action'.$x.'_id']){
-                $this->add('View',null,'Action'.$x,'Action'.$x)->setModel($m->ref('action'.$x.'_id'));
-            }else $this->template->tryDel('Action'.$x);
+        if($m['action2_id']){
+            foreach([1,2] as $x){
+                if($m['action'.$x.'_id']){
+                    $this->add('View',null,'Action'.$x,'Action'.$x)->setModel($m->ref('action'.$x.'_id'));
+                }
+            }
+            $this->template->tryDel('ActionSingle');
+        }else{
+            $this->add('View',null,'ActionSingle','ActionSingle')->setModel($m->ref('action1_id'));
+
+            $this->template->tryDel('ActionMulti');
         }
     }
 }
